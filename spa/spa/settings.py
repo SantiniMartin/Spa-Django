3f59ec9415ee4d2e4fc68af121fafb017b1e97b5
Elias <eliasmorales.21@hotmail.com>
2025-06-26 15:34:39 -0300

    solution

--- spa/spa/settings.py
+++ spa/spa/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'django-insecure-1r-g*ful0no(%rc8p709p&5hvw*1hqgum635ky$#co!hpx)q2$
 #DEBUG = True
 #ALLOWED_HOSTS = []
 
-DEBUG = False
-ALLOWED_HOSTS = ['spa-django.onrender.com']
+DEBUG = True
+ALLOWED_HOSTS = []
 
 
 
